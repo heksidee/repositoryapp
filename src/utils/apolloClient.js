b8a5676d4be7dc9ded0bf930236bdf2f545343d3
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-11-13 15:44:42 +0200

    teht 10.11-10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import fetch from "cross-fetch";
+import Constants from "expo-constants";
 
 const createApolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: "http://192.168.0.184:4000/graphql",
+      uri: Constants.expoConfig.extra.apolloUri,
       fetch,
     }),
     cache: new InMemoryCache(),
